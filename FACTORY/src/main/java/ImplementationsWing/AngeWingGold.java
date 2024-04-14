8309e76e776ce6e4d11fcc66a7eb110c683d8ac8
josue66656 <166780525+josue66656@users.noreply.github.com>
2024-04-13 21:09:43 -0600

    Update AngeWingGold.java

--- FACTORY/src/main/java/ImplementationsWing/AngeWingGold.java
+++ FACTORY/src/main/java/ImplementationsWing/AngeWingGold.java
@@ -7,12 +7,14 @@ public class AngeWingGold implements Enemy{
     // This class is the product result of each identity interface
     @Override
     public void attack() {
-        System.out.println("Fly and Hit Gold");
+        int a = 10;
+        System.out.println("Fly " + a);
     }
 
     @Override
     public void fend() {
-        System.out.println("Throw pieces of wings Gold");
+        int v = 93;
+        System.out.println("Log Main Lap" + 93);
     }
     
 }
